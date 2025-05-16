729b8a5cdfc4a3b0bf5fb0a1c656b630ec4a5ece
Kim Riyeon <kry59965@gmail.com>
2025-05-16 10:27:38 +0900

    초기 커밋

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!\n');
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
+});
