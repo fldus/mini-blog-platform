12835b77be2f825772f7e0eec09123c17a7033f0
Kim Riyeon <kry59965@gmail.com>
2025-05-19 14:43:33 +0900

    기본 설정

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!\n');
+app.use((req, res) => {
+  res.status(404).send('404 not found');
 });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
